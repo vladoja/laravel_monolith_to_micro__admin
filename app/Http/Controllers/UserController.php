7e05e40f4de76d5f793dab2bac9e31c1aa50b952
vladoja <dr.jalo2@gmail.com>
2021-03-09 13:24:46 +0100

    Controller: create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     //
-    public function index() {
+    public function index()
+    {
         return User::all();
+    }
+
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        // $user = User::create($request->all());
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+        return response($user, Response::HTTP_CREATED);
     }
 }
