37eaa992f06a711135e6222713dde2331792e39f
vladoja <dr.jalo2@gmail.com>
2021-03-09 12:46:54 +0100

    User tab.

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/hello', function () {
-    return 'Hello';
-});
+Route::get('/hello', 'UserController@index');
