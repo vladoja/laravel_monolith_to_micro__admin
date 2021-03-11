6f6911c6a3597bf5ebfe331fbfea7d96c593fbde
vladoja <dr.jalo2@gmail.com>
2021-03-11 10:55:05 +0100

    User role_id foreign key

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,5 +23,6 @@ $factory->define(User::class, function (Faker $faker) {
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role_id' => \App\Role::inRandomOrder()->first()->id,
     ];
 });
