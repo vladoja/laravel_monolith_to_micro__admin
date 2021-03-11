19ff7468bbea891cb44458f82ccdf4c60fd86b1f
vladoja <dr.jalo2@gmail.com>
2021-03-11 22:45:40 +0100

    Order and OrderItems

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\OrderItem;
+use Faker\Generator as Faker;
+
+$factory->define(OrderItem::class, function (Faker $faker) {
+    return [
+        'product_title' => $faker->text(30),
+        'price' => $faker->numberBetween(10, 100),
+        'quantity' => $faker->numberBetween(1, 5)
+    ];
+});
