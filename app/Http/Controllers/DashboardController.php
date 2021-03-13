cc34e75eb8536024ce57e3324df476af4c18430e
vladoja <dr.jalo2@gmail.com>
2021-03-13 23:40:19 +0100

    S2_31 Chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ChartResource;
 use App\Order;
 use Illuminate\Http\Request;
 
@@ -9,11 +10,12 @@ class DashboardController extends Controller
 {
     public function chart()
     {
+        \Gate::authorize('view', 'orders');
         $orders = Order::query()->join('order_items', 'orders.id', '=', 'order_items.order_id')
             ->selectRaw("DATE_FORMAT(orders.created_at,'%Y-%m-%d') as date, sum(order_items.price*order_items.quantity) as sum")
             ->groupBy('date')
             ->get();
 
-        return $orders;
+        return ChartResource::collection($orders);
     }
 }
