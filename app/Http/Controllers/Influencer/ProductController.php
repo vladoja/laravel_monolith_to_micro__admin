884720260fa11a162a81c80cb35d4fe8103dc52b
vladoja <dr.jalo2@gmail.com>
2021-04-11 10:24:50 +0200

    S4_90 Caching products.

--- app/Http/Controllers/Influencer/ProductController.php
+++ app/Http/Controllers/Influencer/ProductController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers\Influencer;
 
 use App\Http\Resources\ProductResource;
 use App\Product;
+use Cache;
 use Illuminate\Http\Request;
 
 class ProductController
 {
     public function index(Request $request)
     {
+        $result = Cache::get('products');
+        if ($result) {
+            return $result;
+        }
+        sleep(2);
         $query = Product::query();
         if ($s = $request->input('s')) {
             $query->whereRaw("title LIKE '%{$s}%'")
                 ->orWhereRaw("description LIKE '%{$s}%'");
         }
-        return ProductResource::collection($query->get());
+
+        $response = ProductResource::collection($query->get());
+        Cache::set('products', $response, 5);
+        return $response;
     }
 }
